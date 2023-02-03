dada9bf64d7e6d6199aa91e879e83c97f798cf43
Manav Rupani <ManavStud121@gmail.com>
2023-02-03 16:38:29 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Java Code Executed by Jenkins");
-        System.out.println("Updated Version");
+        System.out.println("Updated Version: February 3");
     }
 }
